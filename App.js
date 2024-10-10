a0821baac7ea1fbcc70d465b1c309105868cd0df
Abhishek_singh <singhrnq231217@gmail.com>
2024-10-10 16:43:04 +0530

    two timer

--- App.js
+++ App.js
@@ -32,36 +32,89 @@
 
 
 
+
+
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState, useEffect } from "react";
 
 const CounterTime = () => {
-  const [sec, setsec] = useState(60);
+  const [sec, setSec] = useState();
   const [start, end] = useState(false);
+  const [sec2, setSec2] = useState();
+  const [start2, end2] = useState(false);
   useEffect(() => {
     let interval;
     if (start) {
-      interval = setInterval(() => setsec((prev) => prev>0?prev-1:0), 1000);
+      interval = setInterval(
+        () => setSec((prev) => (prev > 0 ? prev - 1 : 0)),
+        1000
+      );
     }
     return () => {
       clearInterval(interval);
     };
   }, [start, sec]);
 
-  const handlereset = () => {
-    setsec(60);
+  useEffect(() => {
+    let interval2;
+    if (start2) {
+      interval2 = setInterval(
+        () => setSec2((prev) => (prev > 0 ? prev - 1 : 0)),
+        1000
+      );
+    }
+    return () => {
+      clearInterval(interval2);
+    };
+  }, [start2, sec2]);
+  const handleReset = () => {
+    setSec(0);
     end(false);
+  };
+
+  const handleReset2 = () => {
+    setSec2(0);
+    end2(false);
   };
 
   const handleStart = () => {
     end(!start);
   };
+
+  const handleStart2 = () => {
+    end2(!start2);
+  };
+
   return (
-    <View style={{ flex: 1,margin:50}}>
-      <Text>{sec}</Text>
-      <TextInput placeholder="Enter the time" style={{width:1,borderColor:"black",borderRadius:1}}/>
-      <Button onPress={handlereset} title="reset" />
-      <Button onPress={handleStart} title={start? "end":"start"} />
+    <View style={{ flex: 1, margin: 50 }}>
+      <Text>Timer 1: {sec}</Text>
+      <TextInput
+        value={sec}
+        onChangeText={setSec}
+        placeholder="Enter the time for Timer 1"
+        keyboardType="numeric"
+        style={{ borderWidth: 1, marginBottom: 20 }}
+      />
+      <Button onPress={handleReset} title="Reset" />
+      <Button
+        onPress={handleStart}
+        title={start ? "pause" : "play"}
+      />
+
+
+      <Text style={{ marginTop: 40 }}>Timer 2: {sec2}</Text>
+      <TextInput
+        value={sec2}
+        onChangeText={setSec2}
+        placeholder="Enter the time for Timer 2"
+        keyboardType="numeric"
+        style={{ borderWidth: 1, marginBottom: 20 }}
+      />
+      <Button onPress={handleReset2} title="Reset" />
+      <Button
+        onPress={handleStart2}
+        title={start2 ? "Pause" : "play"}
+      />
     </View>
   );
 };
@@ -69,7 +122,3 @@ const CounterTime = () => {
 export default CounterTime;
 
 const styles = StyleSheet.create({});
-
-
-
-
